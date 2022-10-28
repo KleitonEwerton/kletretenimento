11cf73d52380579a15eee3c9fc1f4c6fa6c14899
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2022-10-28 16:17:11 -0300

    usa js para tentar fazer mudanças no classlist, nao funcional

--- public/js/modalLP.js
+++ public/js/modalLP.js
@@ -1,2 +1,32 @@
 const fade = document.querySelector("#fadeModal");
-const btn = document.querySelector("#add-btn");
+const btn = document.getElementsByClassName("btn");
+
+const modalBtns = [...btn].filter((el)=>{
+    return el.dataset.modal != null;
+})
+
+const toggleModal = (id) =>{
+    if(id == undefined){
+        fade.classList.toggle("hide");
+        const closeModal = closeModal();
+        closeModal.classList.toggle("hide");
+    } else{
+        const openModal = document.getElementById(id);
+        modalOpen.classList.toggle("hide");
+        fade.classList.toggle("hide");
+    }
+}
+
+const closeModal = () =>{
+    const modal = document.getElementById("modalAdd");
+    const openModal = [...modal].filter((modal)=>{
+        return !modal.classList.contains("hide");
+    })
+    openModal[0];
+}
+
+[...modalBtns, fade].forEach((el)=>{
+    el.addEventListener("click",()=>{
+        toggleModal(el.dataset.modal);
+    })
+})
