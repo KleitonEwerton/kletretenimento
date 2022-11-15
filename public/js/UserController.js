b94254525ae7e04219be2e0bdfbdef8724a0e9e1
“LucasSouzaC” <lucasquito2015@gmail.com>
2022-11-15 19:37:15 -0300

    BugdaPaginaDeUsurios

--- public/js/UserController.js
+++ public/js/UserController.js
@@ -5,54 +5,14 @@ class UserController{
         this.register();
     }
 
-    addLine(user){
-        let tr=document.createElement("tr");
-        tr.innerHTML = `
-        <td class ='table-icon'>${user.getId()}</td>
-        <td class ='table-icon''><img src='${user.getPhoto()}' alt ='icone'></td>
-        <td class ='table-icon'>${user.getName()}</td>
-        <td class ='table-icon'>${user.getEmail()}</td>
-        <td class ='table-icon'>${user.getPhone()}</td>
-        <td class ='table-icon'>${user.getDate()}</td>`;
-        if (user.getAdmin()){
-            tr.innerHTML+=`<td class ='table-adimin'>Sim</td>`
-
-        }else{
-            tr.innerHTML+=`<td class ='table-adimin'>Não</td>`
-        }
-        tr.innerHTML+=`
-        <td class ='table-actions'>
-        span class="material-icons-sharp edit-btn">edit</span>
-        span class="material-icons-sharp delete-btn">delete</span>
-        </td>`;
-        document.querySelector('.users tobody').appendChild(tr);
-        
-            
-
-    }
-
-    readPhoto(data){
-
-        return new Promise((resolve,reject)=>{
-            let fr = new FileReader();
-            fr.addEventListener('load',()=>{
-                resolve(fr.result);
-            });
-            fr.addEventListener('error',(e)=>{
-                reject(e)
-            })
-            fr.readAsDataURL(data);
 
 
-        })
-    }
 
 
 
      register(){
         let formEl = document.querySelector('.register');
         let elements = formEl.elements;
-        let user;
         let registerData = {};
          [...elements].forEach((v)=>{
              switch (v.type){
@@ -68,26 +28,7 @@ class UserController{
              }
 
          })
-        if (JSON.stringify(this.users) == JSON.stringify({})){
-            user = new User(0,registerData.name,"",registerData.email,registerData.phone,registerData.admin,registerData.password);
-        }
-        else{
-            let lastUser = Object.values({"a":"teste","b":"teste2"})[Object.values({"a":"teste","b":"teste2"}).length-1];
-            user = new User(lastUser.getId()+1 ,registerData.name, "",registerData.email,registerData.phone,registerData.admin,registerData.password);
-        }
-        let fileEl = elements.photo;
-        if(fileEl.files.length == 0){
-            user.setPhoto("img/icon.jpg");
-        }else {
 
-            this.readPhoto(elements.photo.files[0]).then((result) => {
-                user.setPhoto(result)
-            }, (e) => {
-                console.error(e)
-            })
-
-        }
-         console.log(user)
      }
 
     addEventBtns(){
---
+++ public/js/modalLP.js
@@ -0,0 +1,27 @@
+const fade = document.getElementById("fade");
+const btn = document.getElementsByClassName("btn");
+const container = document.getElementsByClassName("struct-modal");
+
+const modalBtns = [...btn].filter((el)=>{
+    return el.dataset.modal != null;
+})
+
+const toggleModal = (id) =>{
+    if(id == undefined){
+        fade.classList.toggle("hide");
+    } else{
+        const modalOpen = document.getElementById(id);
+        modalOpen.style.display = "block"
+        fade.classList.toggle("hide");
+        fade.addEventListener("click", ()=>{
+            modalOpen.style.display = "none";
+        })
+    }
+}
+
+[...modalBtns, fade].forEach((el)=>{
+    el.addEventListener("click",()=>{
+        toggleModal(el.dataset.modal);
+    })
+})
+
