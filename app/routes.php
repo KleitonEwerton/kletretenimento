61535fcb305c552206e89e325930a7254dcb4afa
Arthur_Riccioppo <arthurriccioppo@gmail.com>
2022-11-17 17:40:19 -0300

    alteraçoes feitas no editusuarios

--- app/routes.php
+++ app/routes.php
@@ -2,5 +2,13 @@
 
 use App\Core\Router;
 
+
 //-----------Rotas do Front-------------//
+
+    $router->get('listaposts', 'PostController@index');
+    $router->get('', 'HomeController@index');
+    
+    
+
+
 
