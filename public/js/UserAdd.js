dd15d4643c380e6f9755431d7696a03512a414bc
“LucasSouzaC” <lucasquito2015@gmail.com>
2022-11-18 14:58:35 -0300

    concertando erro do botão de close

--- public/js/UserAdd.js
+++ public/js/UserAdd.js
@@ -1,17 +1,40 @@
 class UserAdd{
-    constructor() {
+    constructor(){
         this.addEventBtns();
+        this.register();
     }
 
-    addEventBtns() {
-        document.querySelector(".add").addEventListener("click", () => {
+
+    register(){
+        let formEl = document.querySelector('.register');
+        let elements = formEl.elements;
+        let registerData = {};
+        [...elements].forEach((v)=>{
+            switch (v.type){
+                case'checkbox':
+                    registerData.admin = v.checked
+                    break;
+
+                case'file':
+                    break;
+                default:
+                    registerData[v.name] = v.value
+                    break;
+            }
+
+        })
+
+    }
+
+    addEventBtns(){
+
+        document.querySelector(".add").addEventListener("click",()=>{
             document.querySelector(".form-add").style.display = "flex"
         })
 
-        document.querySelectorAll(".close")[0].addEventListener("click", () => {
+        document.querySelectorAll(".close")[0].addEventListener("click",()=>{
             document.querySelector(".form-add").style.display = "none"
         })
-
         document.querySelectorAll(".close")[1].addEventListener("click",()=>{
             document.querySelector(".form-edit").style.display = "none"
         })
