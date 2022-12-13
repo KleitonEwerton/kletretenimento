79d0227cf437f082b30acad7d08e1fb6f1b0ffe6
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2022-12-13 11:18:50 -0300

    reorganiza algumas coisas do site pra resolve um detalhe

--- app/views/site/landinPage.view.php
+++ app/views/site/landinPage.view.php
@@ -18,76 +18,79 @@
 <body>
     <?php include __DIR__ . '/../includes/navbar.php' ?>
 
+    <div class="itens-container">
 
-    <div class="imagem1">
 
-        <h2>
-            <img src="../../../public/assets/logo3-sem-fundo.png" alt="imagen logo">
-        </h2>
+        <div class="imagem1">
 
-    </div>
+            <h2>
+                <img src="../../../public/assets/logo3-sem-fundo.png" alt="imagen logo">
+            </h2>
 
-    <div class="slider">
-        <div class="slides">
-
-            <input type="radio" name="radio-ntn" id="radio1">
-            <input type="radio" name="radio-ntn" id="radio2">
-            <input type="radio" name="radio-ntn" id="radio3">
-
-            <?php
-             $controle = 0; 
-             $classe = 'slide first';
-            ?>
-
-            <?php foreach ($posts as $post) : 
-                if($controle != 0){
-                    $classe = 'slide';   
-                }
-                $controle++;
-            ?>
-            <div class="<?=$classe?>">
-                    <a href='post-visualizacao?id=<?=$post->id?>'><img src="../../../public/assets/<?=$post->imagem?>" alt="<?=$post->tituloDopost?>" ></a>
-            </div>
-            <?php endforeach; ?>
-
-            <div class="navegation-auto">
-                <div class="auto-btn1"></div>
-                <div class="auto-btn2"></div>
-                <div class="auto-btn3"></div>
-
-            </div>
-        
         </div>
 
+        <div class="slider">
+            <div class="slides">
+
+                <input type="radio" name="radio-ntn" id="radio1">
+                <input type="radio" name="radio-ntn" id="radio2">
+                <input type="radio" name="radio-ntn" id="radio3">
+
+                <?php
+                $controle = 0;
+                $classe = 'slide first';
+                ?>
+
+                <?php foreach ($posts as $post) :
+                    if ($controle != 0) {
+                        $classe = 'slide';
+                    }
+                    $controle++;
+                ?>
+                    <div class="<?= $classe ?>">
+                        <a href='post-visualizacao?id=<?= $post->id ?>'><img src="../../../public/assets/<?= $post->imagem ?>" alt="<?= $post->tituloDopost ?>"></a>
+                    </div>
+                <?php endforeach; ?>
+
+                <div class="navegation-auto">
+                    <div class="auto-btn1"></div>
+                    <div class="auto-btn2"></div>
+                    <div class="auto-btn3"></div>
+
+                </div>
+
+            </div>
 
 
-        <div class="manual-navegation">
-            <label for="radio1" class="manual-btn"></label>
-            <label for="radio2" class="manual-btn"></label>
-            <label for="radio3" class="manual-btn"></label>
+
+            <div class="manual-navegation">
+                <label for="radio1" class="manual-btn"></label>
+                <label for="radio2" class="manual-btn"></label>
+                <label for="radio3" class="manual-btn"></label>
+            </div>
+
         </div>
+        <div class="QS-container" id="QuemSomos">
+            <div class="titulo">
+                <h4>Quem somos?</h4>
+            </div>
 
+            <div class="texto">
+                <p>Somos um grupo de universitários da faculdade federal de Juiz de fora, criado com a finalidade de desenvolver um projeto, para o processo treine da empresa júnior code.
+                <p>
+            </div>
+
+
+            <div class="titulo">
+                <h5>O que fazemos?</h5>
+            </div>
+
+            <div class="texto">
+                <p>Adiministramos e desenvolvemos esse blogue destinado a pessoas que procuram por algum programa para o fim de semana, damos esse espaço para que membros da comunidade possam anunciar eventos, festas ou qualquer outra categoria de evento que possa animar um pouco do seu dia, criamos essa página sem fins lucrativos, visando apenas ajudar as pessoas entediadas e sem planos para sair.
+                <p>
+            </div>
+        </div>
     </div>
-
-    <div class="titulo1">
-        <h4>Quem somos?</h4>
-    </div>
-
-    <div class="texto1">
-        <p>Somos um grupo de universitários da faculdade federal de Juiz de fora, criado com a finalidade de desenvolver um projeto, para o processo treine da empresa júnior code.
-        <p>
-    </div>
-
-
-    <div class="titulo2">
-        <h5>O que fazemos?</h5>
-    </div>
-
-    <div class="texto2">
-        <p>Adiministramos e desenvolvemos esse blogue destinado a pessoas que procuram por algum programa para o fim de semana, damos esse espaço para que membros da comunidade possam anunciar eventos, festas ou qualquer outra categoria de evento que possa animar um pouco do seu dia, criamos essa página sem fins lucrativos, visando apenas ajudar as pessoas entediadas e sem planos para sair.
-        <p>
-    </div>
-
 
     <?php include __DIR__ . '/../includes/footer.php' ?>
     <script src="../../../public/js/ScriptLandingPage.js"></script>
